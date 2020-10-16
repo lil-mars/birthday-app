260adcf2a1841174f435641cbbd2a753691ca6de
x-lil-peep-x <ermateze@gmail.com>
2020-10-16 13:49:54 -0400

    Adding Linear gradient.

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <LinearGradient colors={['#A88BEB', '#F8CEEC' ]} style={styles.background}>
+        <View>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto"/>
+        </View>
+      </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  background:{
+    width: '100%',
+    height: '100%'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
